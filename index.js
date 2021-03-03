ce1aff73269ee1d0a113ce8d95792080e6e92849
Anatoly <120422@bk.ru>
2021-03-03 16:52:59 +0700

    новый алгоритм

--- index.js
+++ index.js
@@ -11,74 +11,104 @@ const dataDistances = [
 ];
 
 const delivery = (cities, distances, target_id) => {
-    // Проверяем есть ли город
-    if (!cities.find(city => city.id === target_id)) {
+    // Есть ли город в базе
+    const cityInBase = cities.some(city => city.id === target_id);
+
+    if (!cityInBase) {
         return false;
     }
 
-    // Тут будет маршрут
-    const route = [];
-
-    // Проверяем есть ли товар в выбранном городе
+    // Есть ли товар в этом горде
     const currentCity = cities.find(city => city.id === target_id && city.in_stock > 0);
-    
-    // Если есть возвращаем его id без расстояния
+
     if (currentCity) {
         return currentCity.id;
     }
 
-    // Узнаем id городов в которых товар есть в наличии 
-    const productsInStock = cities.filter(city => city.in_stock > 0).map(city => city.id);
-    // Если нет товара в наличии выходим из функции
-    if (productsInStock.length === 0) {
-        return false;
+    const shortcut = [];
+    const route = [];
+
+    // Собираем массив с которым будем работать
+    cities.forEach(item => {
+        shortcut.push({
+            id: item.id, // идентификатор горда
+            in_stock: item.in_stock, // количество товара в городе
+            distance: item.id === target_id ? 0 : Infinity, // расстояние от города до цели
+            is_checked: false, // проверен ли город
+        });
+    });
+
+    // Помещаем целевой город на первое место в массиве
+    shortcut.sort((a, b) => a.distance - b.distance);
+
+    // Узнаем расстояние от цели до городов
+    const findDistance = (target) => {
+        // Находи соседние города в порядке увеличения пути
+        const cityNeighbors = distances.filter(e => e.dst_city_id === target)
+            .sort((a, b) => a.distance - b.distance);
+
+        // Записываем расстояние до каждого соседнего города
+        cityNeighbors.forEach(item => {
+            // Расстояние от города до цели
+            const distanceToThatCity = shortcut.find(e => e.id === target).distance;
+
+            // Этот город в массиве shortcut
+            const thatCity = shortcut.find(e => e.id === item.src_city_id);
+
+            // Записываем расстояние до города если оно меньше чем было
+            thatCity.distance = (item.distance + distanceToThatCity) < thatCity.distance
+                ? item.distance + distanceToThatCity
+                : thatCity.distance;
+        });
+        // Помечаем город с которым работали
+        const checkedCity = shortcut.find(e => e.id === target);
+        checkedCity.is_checked = true;
+
+        // Если все города проверены строим маршрут до цели
+        if (!shortcut.some(e => !e.is_checked)) {
+            // Находим ближайший город с товаром в наличии
+            const cityWithInStock = shortcut.filter(e => e.in_stock > 0)
+                .sort((a, b) => a.distance - b.distance)[0];
+
+            // Строим маршрут до цели
+            const buildRoute = (city) => {
+                // Выходим из функции если дошли до цели
+                if (city.id === target_id) {
+                    return false;
+                }
+
+                let nextCity;
+                // Находим соседние города
+                const cityNeighbors = distances.filter(e => e.src_city_id === city.id);
+
+                cityNeighbors.forEach(item => {
+                    // Если расстояние до соседнего города равно расстоянию из массива shortcut, т.е. кратчайшему
+                    if (city.distance - item.distance === shortcut.find(e => e.id === item.dst_city_id).distance) {
+                        nextCity = shortcut.find(e => e.id === item.dst_city_id);
+                        // Запоминаем эту часть маршрута
+                        route.push(item);
+                    }
+                });
+                // Переходим к следующей части маршрута
+                buildRoute(nextCity);
+            }
+
+            buildRoute(cityWithInStock)
+
+            return false
+        }
+        // Находим следующий город и узнаем расстояние до него
+        const nextPonitIndex = shortcut.indexOf(shortcut.find(e => e.id === target)) + 1;
+        findDistance(shortcut[nextPonitIndex].id);
     }
 
-    // Функция которая создает маршрут доставки
-    const router = (productsInStock, id) => {
-        // Функция которая находит самое короткое расстояне
-        const smallestDistance = (arr) => {
-            const smallestDistance = arr.map(e => e.distance).sort((a, b) => a - b)[0];
-            return arr.find(item => item.distance === smallestDistance);
-        }
-
-        // Узнаем из какого горда можно доставить товар
-        const withDelivery = distances.filter(city => city.dst_city_id === id);
-        // Если нет доставки в этот город выходим из функции
-        if (withDelivery.length === 0) {
-            return false;
-        }
-
-        // Находим города с доставкой и товаром в наличии
-        const productsAndDelivery = withDelivery.filter(item => productsInStock.indexOf(item.src_city_id) >= 0);
-
-        // Если нет города из которого есть прямая доставка...
-        if (productsAndDelivery.length === 0) {
-            // находим город в котором есть доставка в город из которого товар можно доставить покупатель
-            const id = smallestDistance(withDelivery).src_city_id;
-            router(productsInStock, id);
-            // Добовляем город в маршрут
-            route.push(withDelivery[0]);
-        } else {
-            // Если есть города с прямой доставкой выбираем ближайший
-            smallestDistance(productsAndDelivery);
-            // Добовляем город в маршрут
-            route.push(productsAndDelivery[0]);
-        }
-    }
-
-    // Строим маршрут до нужного города
-    router(productsInStock, target_id);
-
-    if (route.length === 0) {
-        return false;
-    }
+    findDistance(target_id);
 
     return {
-        id: route[0].src_city_id,
-        distance: route.map(e => e.distance).reduce((a, b) => a + b),
-        route,
-    };
+        id: route[0].src_city_id, // id города из которого будет произведена доставка
+        distance: shortcut.find(e => e.id === route[0].src_city_id).distance, // расстояние до цели
+        route, // маршрут доставки
+    }
 }
 
 const response = delivery(dataCities, dataDistances, 2);
